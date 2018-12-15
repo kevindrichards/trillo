fd1468da7a0d436347596d648bf82cc5a8fcc792
kevindrichards <kevin.d.richards@hotmail.com>
2018-12-15 17:46:04 -0500

    move html to public

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var app = express();
 app.use(express.static(__dirname + "/public"));
 
 app.get("/", function(req, res){
-    res.send("index.html");
+    res.render("index.html");
 });
 
 app.listen(process.env.PORT, process.env.IP, function(){
