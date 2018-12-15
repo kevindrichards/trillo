8669821ee98b6f9618bb89eb9b16da1b4cc84c1c
kevindrichards <kevin.d.richards@hotmail.com>
2018-12-15 17:29:03 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+var express = require("express");
+var app = express();
+
+app.use(express.static(__dirname + "/public"));
+
+app.get("/", function(req, res){
+    res.send("index.html");
+});
+
+app.listen(process.env.PORT, process.env.IP, function(){
+    console.log("Server Has Started");
+});
